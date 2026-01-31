d4369886c3a82b479148cffdd4fccb838325723f
Thanakorn004 <tnkzxza@gmail.com>
2026-01-31 14:24:58 +0700

    Conditional Statements

---
+++ python4.py
@@ -0,0 +1,20 @@
+"""
+#
+# Conditional Statements 
+#
+"""
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a <= b:
+    print("a is greater than b")
+else:
+    print("a is greater than b")
+
+if a < b:
+    print("a is less than b")
+if a == b:
+    print("a is equal to b")
