652e994fd02fba4c4351cc03f98cb9cf5fcded02
Thanakorn004 <tnkzxza@gmail.com>
2026-01-31 13:46:20 +0700

    make a .gitignore

--- python2.py
+++ python2.py
@@ -10,3 +10,30 @@ z = 3 * 7
 a = 8 / 2
 b = 10 % 3
 c = 2 ** 3
+
+"""
+#
+# Part : Python Operators
+# Assignment Operators
+#
+"""
+
+x = 5
+x += 3
+x = x + 3
+
+"""
+#
+# Part : Python Operators
+# Comparison Operators
+#
+"""
+a = 5
+b = 10
+print (a == b)
+print (a != b)
+print (a > b)
+print (a < b)
+print (a >= b)
+print (a <= b)
+
